90b9b75b03e2dc0c0a9bec19a88429580350768d
Nuzaili-design <abal-nuzaili@stu.okan.edu.tr>
2025-08-08 05:06:57 +0300

    Initial commit for minified version

---
+++ script.js
@@ -0,0 +1,26 @@
+ let typingEffect = new Typed(".typedText", {
+        strings: ["Data Analyst", "Power BI", "SQL"],
+        loop: true,
+        typeSpeed: 100,
+        backSpeed: 80,
+        backDelay: 2000
+    });
+const sr = ScrollReveal({
+  origin: 'left',      // All from the left
+  distance: '80px',    // Slightly more dramatic scroll
+  duration: 1500,
+  delay: 300,          // Unified delay
+  reset: false
+});
+
+// Apply to all relevant sections
+sr.reveal('.featured-text-card');
+sr.reveal('.featured-name');
+sr.reveal('.featured-text-info');
+sr.reveal('.top-header');
+sr.reveal('.about-info');
+sr.reveal('.project-box');
+sr.reveal('.contact-info');
+sr.reveal('.project-box', {
+  interval: 200
+});
